18c168c59aae1a9c43894e9a1dfcdd115663fd05
anibal1015 <128444208+anibal1015@users.noreply.github.com>
2023-06-13 11:41:51 -0500

    Update TestUserRepositoryImpl.java

--- taller-7-main/project_2687365/src/main/java/co/edu/sena/project_2687365/model/repository/TestUserRepositoryImpl.java
+++ taller-7-main/project_2687365/src/main/java/co/edu/sena/project_2687365/model/repository/TestUserRepositoryImpl.java
@@ -13,17 +13,17 @@ public class TestUserRepositoryImpl {
 
         System.out.println("========saveObj Insert===========");
         User userInsert1 = new User();
-        userInsert1.setUser_firstname("Juan");
-        userInsert1.setUser_lastname("Abreu");
-        userInsert1.setUser_email("jjk@gmail.com");
-        userInsert1.setUser_password("siabreu21");
+        userInsert1.setUser_firstname("Jose");
+        userInsert1.setUser_lastname("Gomez");
+        userInsert1.setUser_email("joae@gmail.com");
+        userInsert1.setUser_password("gamoe54");
         repository.saveObj(userInsert1);
 
         User userInsert2 = new User();
-        userInsert2.setUser_firstname("Juanjo");
-        userInsert2.setUser_lastname("Abreo");
-        userInsert2.setUser_email("jk@gmail.com");
-        userInsert2.setUser_password("noabreu21");
+        userInsert2.setUser_firstname("Anival");
+        userInsert2.setUser_lastname("Novoa");
+        userInsert2.setUser_email("aniv@gmail.com");
+        userInsert2.setUser_password("astrid85");
         repository.saveObj(userInsert2);
 
 
@@ -37,10 +37,10 @@ public class TestUserRepositoryImpl {
 
         System.out.println("============== saveObj ===========");
         User userUpdate = new User();
-        userUpdate.setUser_firstname("jose");
-        userUpdate.setUser_lastname("ramo");
-        userUpdate.setUser_email("jora@gmail.com");
-        userUpdate.setUser_password("21noramo");
+        userUpdate.setUser_firstname("andres");
+        userUpdate.setUser_lastname("Bastidas");
+        userUpdate.setUser_email("anba@gmail.com");
+        userUpdate.setUser_password("tidas897");
         repository.listAllObj().forEach(System.out::println);
         System.out.println();
 
